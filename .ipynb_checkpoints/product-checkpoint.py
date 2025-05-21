347660c2c20a6216e1e70adb2659270ecd2a9f5d
cahyareza <>
2025-05-21 08:35:12 +0700

    initial commit

---
+++ .ipynb_checkpoints/product-checkpoint.py
@@ -0,0 +1,8 @@
+class Product:
+    _product_id = 100902
+    _product_name = 'Iphone X'
+    _product_category = 'Electronics'
+    _unit_price = 700
+    
+    def get_product(self):
+        return self._product_id, self._product_name, self._product_category, self._unit_price
